3b70d38736e2eb9f36c871b82d21bb7c58ae0f65
gabriel <gaamdal557@gmail.com>
2023-03-17 13:19:16 -0300

    models

---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace APICatalogo.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImageUrl { get; set; }
+}
