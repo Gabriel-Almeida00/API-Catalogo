857b37c95a4245b3bad899e2b43cd5c4d18ddfb5
gabriel <gaamdal557@gmail.com>
2023-03-21 19:43:38 -0300

    unit of work

---
+++ APICatalogo/RepositoryImpl/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Repository;
+using APICatalogo.RepositoryImpl;
+
+namespace APICatalogo.Implementation
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ProdutoRepository produtoRepository;
+        private CategoriaRepository categoriaRepository;
+        public AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IProdutoRepository ProdutoRepository
+        {
+            get
+            {
+                return produtoRepository = produtoRepository ?? new ProdutoRepository(_context);
+            }
+        }
+
+        public ICategoriaRepository CategoriaRepository
+        {
+            get
+            {
+                return categoriaRepository = categoriaRepository ?? new CategoriaRepository(_context);
+            }
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
