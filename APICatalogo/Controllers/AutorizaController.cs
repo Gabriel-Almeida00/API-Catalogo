4375667722045d7d062cfb3ae1bf5965eb2b2ce4
gabriel <gaamdal557@gmail.com>
2023-03-25 14:48:02 -0300

    autoriza controller registro e login

---
+++ APICatalogo/Controllers/AutorizaController.cs
@@ -0,0 +1,64 @@
+﻿using APICatalogo.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AutorizaController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AutorizaController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "AutorizaController :: Acessado em : "
+                + DateTime.Now.ToLongDateString();
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> RegisterUser([FromBody] UsuairoDTO usuairoDTO)
+        {
+            var user = new IdentityUser
+            {
+                UserName = usuairoDTO.Email,
+                Email = usuairoDTO.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, usuairoDTO.Password);
+
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+
+            await _signInManager.SignInAsync(user, false);
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] UsuairoDTO usuairoDTO)
+        {
+            var result = await _signInManager.PasswordSignInAsync(usuairoDTO.Email,
+                usuairoDTO.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Login inválido....");
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
---
+++ APICatalogo/DTOs/UsuairoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace APICatalogo.DTOs
+{
+    public class UsuairoDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
